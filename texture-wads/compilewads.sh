fd4c605af72f51111ba34a90df22947b97778112
lavender <misslav@protonmail.com>
2022-02-03 01:23:30 -0600

    texture dir's are back in

--- texture-wads/compilewads.sh
+++ texture-wads/compilewads.sh
@@ -1,11 +1,12 @@
+qpakman lq_dev/*.png -o lq_dev.wad
 qpakman lq_extra/*.png -o lq_extra.wad
 qpakman lq_metal/*.png -o lq_metal.wad
 qpakman lq_tech/*.png -o lq_tech.wad
 qpakman lq_mayan/*.png -o lq_mayan.wad
 qpakman lq_liquidfall/*.png -o lq_liquidfall.wad
-qpakman lq_eye/*.png -o lq_eye.wad
 qpakman lq_greek/*.png -o lq_greek.wad
 qpakman lq_health_ammo/*.png -o lq_health_ammo.wad
 qpakman lq_medieval/*.png -o lq_medieval.wad
-qpakman lq_utility/*.png -o lq_utility.wad
 qpakman lq_freedoom/*.png -o lq_freedoom.wad
+
+
