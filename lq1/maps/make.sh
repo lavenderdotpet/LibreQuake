e04c4f2fe9cdc0addb816792a23fceb98a3d4428
MissLavender-LQ <MissLav@protonmail.com>
2023-08-23 07:19:39 +0100

    Update make.sh

--- lq1/maps/make.sh
+++ lq1/maps/make.sh
@@ -1,52 +1,81 @@
+### make commands ###
 function make {
     cd src/ ;
-    for f in $(find . -name '*.map') ; do qbsp "$f" ; done ; 
-    for f in $(find . -name '*.map') ; do vis "$f" ; done ; 
-    for f in $(find . -name '*.map') ; do light -extra "$f" ; done ;
+    for f in $(find . -name '*.map') ; do qbsp "$f" ; done ;
+    for f in $(find . -name '*.map') ; do vis "$f" ; done ;
+    for f in $(find . -name '*.map') ; do light -extra4 "$f" ; done ;
     cp *.bsp .. ; 
     cp *.log logs/ ;
     cp *.lit .. ;
     rm -f *.log *.bsp *.lit;
-    echo -n "";
-    echo -n "=====Build done=====";
-    echo -n "";
+    echo -e "=====Build done=====";
 }
 
+function -m {
+    cd src/ ;
+    for f in $(find . -name '*.map') ; do qbsp "$f" ; done ;
+    for f in $(find . -name '*.map') ; do vis "$f" ; done ;
+    for f in $(find . -name '*.map') ; do light -extra4 "$f" ; done ;
+    cp *.bsp .. ; 
+    cp *.log logs/ ;
+    cp *.lit .. ;
+    rm -f *.log *.bsp *.lit;
+    echo -e "=====Build done=====";
+}
+
+### clean commands ###
 function clean {
     cd src/ ;   
     rm -f *.prt *.texinfo *.pts;
-    echo -n "";
-    echo "=====Cleaning done=====";
-    echo -n "";
+    echo -e "=====Cleaning done=====";
+}
+
+function -c {
+    cd src/ ;   
+    rm -f *.prt *.texinfo *.pts;
+    echo -e "=====Cleaning done=====";
+}
+
+### help commands ###
+function help {
+    echo -e "
+    ==========================================================================
+    Dependencies [ericw-tools][https://github.com/ericwa/ericw-tools/]
+    run ./make.sh make or -m to compile all maps
+    run ./make.sh clean or -c to remove extra files that are left after build
+    run ./make.sh help or -h to read this msg :3
+    ==========================================================================
+    ";
 }
 
 function -h {
-    echo -n "";
-    echo "
-========================================================================
-    run ./make.sh to compile all maps
-    run ./make.sh clean to remove extra files that are left after build
+    echo -e "
+    ==========================================================================
+    Dependencies [ericw-tools][https://github.com/ericwa/ericw-tools/]
+    run ./make.sh make or -m to compile all maps
+    run ./make.sh clean or -c to remove extra files that are left after build
     run ./make.sh help or -h to read this msg :3
-========================================================================
+    ==========================================================================
     ";
-    echo -n "";
 }
 
-function help {
-    echo -n "";
-    echo "
-========================================================================
-    run ./make.sh to compile all maps
-    run ./make.sh clean to remove extra files that are left after build
-    run ./make.sh help or -h to read this msg :3
-========================================================================
-    ";
-    echo -n "";
-}
 
+### command list ###
 case $1 in
+    make)
+    make
+    ;;
+
+    -m)
+    -m
+    ;;
+
     clean)
     clean
+    ;;
+    
+    -c)
+    -c
     ;;
 
     help)
@@ -58,7 +87,11 @@ case $1 in
     ;;
 
     *)
-    make
+    echo -e "
+    ====================================
+     run ./make.sh help or ./make.sh -h
+    ====================================
+    "
     ;;
     
 esac
