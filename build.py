b13b8e0b74a391d88480b5d6c9f1fc133e7caa1b
ZungrySoft <ZungrySoftEntertainment@gmail.com>
2023-12-08 17:13:24 -0800

    Removed wads from repo

--- build.py
+++ build.py
@@ -1,11 +1,22 @@
 # Code written by ZungryWare
 
+# This script copies the necessary files to build the different releases. The
+# releases are specified in build_releases.json and they specify which
+# components to use. Components are specified in build_components.json and each
+# one is a big list of files that a release either wants all of or none of. Each
+# component also specifies whether each file should end up in PAK0.PAK,
+# PAK1.PAK, or just in the root folder without being packed. Files are copied to
+# the 'working' directory, compiled, then copied over to a folder in the
+# 'releases' directory. Before doing this, the script also compiles all of the
+# wad and bsp/lit files so they can be copied over.
+
 import subprocess
 import os
 import json
 import shutil
 import warnings
 
+# Builds a single file
 def build_file(source_path, destination_path, source_if_missing):
     print(source_path)
     os.makedirs(os.path.dirname(destination_path), exist_ok=True)
@@ -17,12 +28,16 @@ def build_file(source_path, destination_path, source_if_missing):
         else:
             warnings.warn(f"Missing file with no substitute: {source_path}")
 
+# Gets a source and dest path from a file entry File. Entries can be a string
+# (where source and dest are the same) or a 2-long list (specifying the source
+# and dest, respectively)
 def extract_file(file):
     if type(file) is list:
         return file[0], file[1]
     else:
         return file, file
 
+# Builds a single component from build_components.json
 def build_component(component_data):
     # Determine the source if missing
     # This is the file to use as a substitute if the desired file is missing
@@ -55,6 +70,7 @@ def build_component(component_data):
             source_if_missing,
         )
 
+# Build a single release from build_releases.json
 def build_release(name, data):
     # Print
     print(f"Building release {name}...")
@@ -82,13 +98,14 @@ def build_release(name, data):
     if pak0_exists:
         subprocess.call('qpakman * -o ../PAK0.PAK', cwd='working/pak0')
         shutil.copy('working/PAK0.PAK', os.path.join('releases', name, base_dir, 'PAK0.PAK'))
-    
+
     # Build and copy pak1
     pak1_exists = len(os.listdir('working/pak1')) > 0
     if pak1_exists:
         subprocess.call('qpakman * -o ../PAK1.PAK', cwd='working/pak1')
         shutil.copy('working/PAK1.PAK', os.path.join('releases', name, base_dir, 'PAK1.PAK'))
 
+# Clears the working directory and sets up empty directories
 def clear_working():
     # Remove working folder and its contents
     shutil.rmtree('./working', ignore_errors=True)
@@ -99,14 +116,20 @@ def clear_working():
     os.makedirs(f'working/pak1', exist_ok=True)
     os.makedirs(f'working/unpacked', exist_ok=True)
 
+def compile_wad():
+    subprocess.call(['sh', 'compilewads.sh'], cwd='texture-wads')
+
 def main():
+    # First, compile wads
+    compile_wad()
+
     # Delete existing releases
-    shutil.rmtree('./releases', ignore_errors=True)
+    # shutil.rmtree('./releases', ignore_errors=True)
 
     # Build each release one by one
-    with open('build_releases.json', 'r') as json_file:
-        releases = json.load(json_file)
-        for key, value in releases.items():
-            build_release(key, value)
+    # with open('build_releases.json', 'r') as json_file:
+    #     releases = json.load(json_file)
+    #     for key, value in releases.items():
+    #         build_release(key, value)
 
 main()
