349e0ef4b21877a71da929f0521ff7905f9e07c6
MissLavender-LQ <misslav@protonmail.com>
2024-03-04 23:31:26 +0000

    lq_palette.wad added to script

--- texture-wads/compilewads.sh
+++ texture-wads/compilewads.sh
@@ -8,6 +8,7 @@ qpakman lq_liquidsky/*.png -o lq_liquidsky.wad
 qpakman lq_mayan/*.png -o lq_mayan.wad
 qpakman lq_medieval/*.png -o lq_medieval.wad
 qpakman lq_metal/*.png -o lq_metal.wad
+qpakman lq_palette/*.png -o lq_palette.wad
 qpakman lq_props/*.png -o lq_props.wad
 qpakman lq_tech/*.png -o lq_tech.wad
 qpakman lq_terra/*.png -o lq_terra.wad
