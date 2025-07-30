887b4d38a405523d7bb94d68168d7de51a7d0bdf
RandomBrushes <brushes@mailfence.com>
2025-07-30 19:38:56 +0200

    AUDIO: Better programming style for convert-music.sh

--- lq1/music/convert-music.sh
+++ lq1/music/convert-music.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 FFMPEG="ffmpeg"
-FFMPEG_OPTS="-ar 44100 -ac 2 -c:a libvorbis -q:a 2 -y -loglevel quiet"
-SRCDIR="./src/"
+FFMPEG_OPTS=(
+    "-ar" "44100"
+    "-ac" "2"
+    "-c:a" "libvorbis"
+    "-q:a" "2"
+    "-y"
+    "-loglevel" "quiet"
+)
+SRCDIR="./src"
 
+# Declare associative array (key: output filename, value: source filename)
 declare -A MUSICFILES
 MUSICFILES["track02"]="track02.ogg"
 MUSICFILES["track03"]="track03.ogg"
@@ -16,13 +24,24 @@ MUSICFILES["track09"]="track09.ogg"
 MUSICFILES["track10"]="track10.ogg"
 MUSICFILES["track11"]="track11.ogg"
 
-
 for dst in "${!MUSICFILES[@]}"; do
-    src=${MUSICFILES[$dst]}
+    src="${MUSICFILES[$dst]}"
+    output="${dst}.ogg"
 
     echo "converting music track $dst..."
 
-    command="${FFMPEG} -i ${SRCDIR}${src} ${FFMPEG_OPTS} ${dst}.ogg"
+    # Build command as array
+    ffmpeg_cmd=(
+        "$FFMPEG"
+        "-i" "${SRCDIR}/${src}"
+        "${FFMPEG_OPTS[@]}"
+        "$output"
+    )
 
-    $command
+    # Execute command
+    if ! "${ffmpeg_cmd[@]}"; then
+        echo "Error converting $src" >&2
+        exit 1
+    fi
 done
+
